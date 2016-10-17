85ca1ae5acf71a33b281e5059f1334f0fbe57072
TakuKobayashi <takobayashi@ncjapan.co.jp>
2016-10-17 17:01:44 +0900

    動きが怪しいものに関しては一旦外すようにしました。その上で再構築しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tools, only: [:index] do
+    collection do
+      get 'draw'
+    end
+  end
 end
